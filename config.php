158238aeaebcf130794e95cb570f16b1dcc2e75e
IshiiMasako <C200020@ccg.ac.jp>
2022-01-20 02:48:25 +0000

    first commit 1/20

--- config.php
+++ config.php
@@ -8,8 +8,11 @@ define('DB_USER','root');
 define('DB_PASS','');
 /* PHPのエラーを表示するように設定 */
 error_reporting(E_ALL & ~E_NOTICE);
-/* */
-define('PWD_LENGTH',4);   // パスワード長
+
+/*１）パスワード文字数制限を4文字以上から5文字以上に変更する（5文字未満はエラー） 
+修正ファイル：ヒント無し 
+修正箇所：ヒント無し */
+define('PWD_LENGTH',5);   // パスワード長
 define('PAGING',5);      // ページネーション単位
 
 
